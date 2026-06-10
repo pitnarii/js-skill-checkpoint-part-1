b68cc972292193ef456eaa292b9426f7cb8328af
pitnaree <pitnaree_@outlook.com>
2026-06-10 13:37:35 +0700

    feat: add answer to q1 JS checkpoint

--- question-1.js
+++ question-1.js
@@ -6,3 +6,16 @@ const myTodo = [
   { id: 4, todo: "Wash dishes" },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+/*add object id:5, todo:"Walk the dog"
+edit object id: 4 todo:"Go to the gym"
+delete last ob in array
+console.log
+bring last obj and change to "To-do id: <ค่าของ id>, <ค่าของ todo>" then console.log
+*/ 
+function editTodo(myTodo) {
+  myTodo.push({id: 5, todo: "Walk the dog"});
+  myTodo[3].todo = "Go to the gym";
+  delete myTodo[4]
+  return `To-do id: ${myTodo[3].id}, ${myTodo[3].todo}`
+}
+console.log(editTodo(myTodo))
