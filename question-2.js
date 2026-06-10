f3dde913216051e272dfe02ac5dc2f9ef9b66e05
pitnaree <pitnaree_@outlook.com>
2026-06-10 14:01:26 +0700

    feat: answer q2

--- question-2.js
+++ question-2.js
@@ -4,3 +4,21 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+/*
+-edit quantity of Apples from 100 => 200;
+-add new item "orange" to inventory, price: 20, quantity: 300
+-find a total value of inventory: Total(amount*price) 
+-console.log("มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก 13500 บาท")  
+*/
+
+inventory[0].quantity = 200;
+inventory.push({ name: "Orange", price: 20, quantity: 300 });
+console.log(inventory);
+function sumOfInventory(inventory) {
+  let totalPrice = 0
+  for (let items of inventory) {
+    totalPrice += items.quantity * items.price
+  }
+  return `มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก ${totalPrice} บาท`;
+}
+console.log(sumOfInventory(inventory));
